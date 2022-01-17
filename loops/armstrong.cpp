ad6ca3667d76789645b9dd2715e78bdb3fbefc73
Shruti Mishra <shrutidmishra2002@gmail.com>
2022-01-18 01:21:26 +0530

    added:armstrong no program to folder loops

--- loops/armstrong.cpp
+++ loops/armstrong.cpp
@@ -1,6 +1,31 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
-    
+    // int i;
+    int i, rem, temp, ans = 0;
+    int no;
+    cout << "enter no"<< " ";
+    cin >> no;
+    temp = no;
+    // both for and while loop working 
+
+    for(i=no;no>0;i++)
+    // while (no > 0)
+    {
+        rem = no % 10;
+        ans = ans + (rem * rem * rem);
+        no = no / 10;
+    }
+    if (ans == temp)
+    {
+        cout << "armstrong no"<<" " << temp;
+    }
+    else
+    {
+        cout << "not a armstrong no" << " " << temp;
+    }
+    return 0;
 }
+// pow(3,no)
